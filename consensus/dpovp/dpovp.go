29e21d5520faf2a8b2444dc91963c0be53f690c6
sman <sman2013@163.com>
2018-05-25 17:38:54 +0800

    for block reward

--- consensus/dpovp/dpovp.go
+++ consensus/dpovp/dpovp.go
@@ -21,6 +21,14 @@ import (
 	"github.com/LemoFoundationLtd/lemochain-go/rpc"
 )
 
+// Ethash proof-of-work protocol constants.
+var (
+	FrontierBlockReward    *big.Int = big.NewInt(5e+18) // Block reward in wei for successfully mining a block
+	ByzantiumBlockReward   *big.Int = big.NewInt(3e+18) // Block reward in wei for successfully mining a block upward from Byzantium
+	maxUncles                       = 2                 // Maximum number of uncles allowed in a single block
+	allowedFutureBlockTime          = 15 * time.Second  // Max time from current time allowed for blocks, before they're considered future blocks
+)
+
 type Dpovp struct {
 	config *params.DpovpConfig // Consensus engine configuration parameters
 	db     lemodb.Database     // Database to store and retrieve snapshot checkpoints
@@ -266,6 +274,7 @@ func (d *Dpovp) Prepare(chain consensus.ChainReader, header *types.Header) error
 func (d *Dpovp) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction,
 	uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
 	// No block rewards in PoA, so the state remains as is and uncles are dropped
+	accumulateRewards(chain.Config(), state, header, uncles)
 	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
 	header.UncleHash = types.CalcUncleHash(nil)
 
@@ -319,4 +328,35 @@ func (d *Dpovp) CalcDifficulty(chain consensus.ChainReader, time uint64, parent 
 // APIs returns the RPC APIs this consensus engine provides.
 func (d *Dpovp) APIs(chain consensus.ChainReader) []rpc.API {
 	return nil
+}
+
+// Some weird constants to avoid constant memory allocs for them.
+var (
+	big8  = big.NewInt(8)
+	big32 = big.NewInt(32)
+)
+
+// AccumulateRewards credits the coinbase of the given block with the mining
+// reward. The total reward consists of the static block reward and rewards for
+// included uncles. The coinbase of each uncle block is also rewarded.
+func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header *types.Header, uncles []*types.Header) {
+	// Select the correct block reward based on chain progression
+	blockReward := FrontierBlockReward
+	//if config.IsByzantium(header.Number) {
+	//	blockReward = ByzantiumBlockReward
+	//}
+	// Accumulate the rewards for the miner and any included uncles
+	reward := new(big.Int).Set(blockReward)
+	r := new(big.Int)
+	for _, uncle := range uncles {
+		r.Add(uncle.Number, big8)
+		r.Sub(r, header.Number)
+		r.Mul(r, blockReward)
+		r.Div(r, big8)
+		state.AddBalance(uncle.Coinbase, r)
+
+		r.Div(blockReward, big32)
+		reward.Add(reward, r)
+	}
+	state.AddBalance(header.Coinbase, reward)
 }
