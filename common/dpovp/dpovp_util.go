7a34df855aa167dbfb5f0a6e34688384df38d53e
lnk <wyy102489@outlook.com>
2018-06-01 15:33:27 +0800

    refactor: core nodes configuration

--- common/dpovp/dpovp_util.go
+++ common/dpovp/dpovp_util.go
@@ -22,16 +22,18 @@ func GetAddressByPubkey(pubKey *ecdsa.PublicKey) common.Address {
 func GetAllSortedCoreNodes() []AddrNodeIDMapping {
 	// TODO
 	result := make([]AddrNodeIDMapping, 1)
-	addr1 := common.HexToAddress(`0xb2a6935aedca3c64d1a98787f48ef4bc010b09d7`)
-	pubkey1:=[]byte(`fa609f0ff5d528f74e013be7a07a4c1365ba5e288ef5ae043037f9f3edaa02e72dbb2d860e01d67a99c289e307250cea4918b8c18377b7fad5d015d90ea90b2a`)
-	tmp1 := AddrNodeIDMapping{addr1,pubkey1 }
+	var addr = common.HexToAddress(`0xb2a6935aedca3c64d1a98787f48ef4bc010b09d7`)
+	var pubKey = []byte(`8e88095f606e713bb52be8268ab7fe94f9656c0cf7a86d02dcaf141c7d54ed5ae9a3eaf69a2209a2fd645b33819df9ac46f4c251269671a847a7b01adf24b517`)
+	result = append(result, AddrNodeIDMapping{addr, pubKey})
 
-	//addr2 := common.HexToAddress(`0x2fb44abed468e558ffcb2ef3de03c7746038be04`)
-	//pubkey2:= []byte(`03b7e35a9dcb07e2e90a4eddfd711d203e9cdd84d8f1dc2b495edbdf264caf37b19e2e2ee43aa59287f3a8a45e212e24ffc091c71f6ddadbd0bc2baae9c8e00a`)
-	//tmp2 := AddrNodeIDMapping{addr2,pubkey2 }
+	addr = common.HexToAddress(`0xc848fe1d6b93c0ec640b3b2469c40378c8adbb5a`)
+	pubKey = []byte(`0cfb8b4d451fe60b86e4c258632e05e44c044d29bce69a3148b7949ace4320804b95f313c4e9cb69c93861fdbee62177d0d30bda1663c2f580bb6bdd196b244b`)
+	result = append(result, AddrNodeIDMapping{addr, pubKey})
 
-	result[0] = tmp1
-	//result[1] = tmp2
+	addr = common.HexToAddress(`0x8353a1ce6b1a77a6863de1e0a764a6f3e58d3b0b`)
+	pubKey = []byte(`b25236b650906006dcb32f44aee565e54fb83026ce6a5d913ed7063a2a9e0c5b8e0a913b5755d6094fe36edaec73031a2f90f13992b6ae18c57520a8de094bfd`)
+	result = append(result, AddrNodeIDMapping{addr, pubKey})
+
 	return result
 }
 
